9f8cbeaf31a752ab97e7845b428031acebde7eec
taachiwi <taachiwi@gmail.com>
2022-09-02 14:05:40 -0300

    show page

--- app.js
+++ app.js
@@ -42,6 +42,14 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(__dirname + "/dist/"));
+  app.use('/api/v1', express.static(path.join(__dirname, '/dist/')))
+  app.get("/*/", (req, res) => {
+    res.sendFile(__dirname + "/dist/index.html");
+  });
+}
+
 // File uploading
 app.use(
   fileupload({
